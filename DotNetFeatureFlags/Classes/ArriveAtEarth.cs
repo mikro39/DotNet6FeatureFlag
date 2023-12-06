fe8d8fa476a1ae325d3e098b447d51a2030680b3
Mike Romero <mikro39@gmail.com>
2023-12-06 15:27:08 -0700

    This is the first

---
+++ DotNetFeatureFlags/Classes/ArriveAtEarth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFeatureFlags.Classes
+{
+    public class ArriveAtEarth
+    {
+        public void EarthArrival()
+        {
+            Console.WriteLine("Welcome to Earth!");
+        }
+    }
+}
---
+++ DotNetFeatureFlags/Classes/ArriveAtMars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFeatureFlags.Classes
+{
+    public class ArriveAtMars
+    {
+        public void MarsArrival()
+        {
+            Console.WriteLine("Welcome to Mars!");
+        }
+    }
+}
